3d3fd6e233499a9daaa44f0f57a6d6eb8e670967
Moibe <moi.estrello@gmail.com>
2025-03-06 19:05:59 -0600

    local_check

--- herramientas.py
+++ herramientas.py
@@ -1,15 +1,18 @@
 import os
 import socket
 from openai import OpenAI
+import time
 
 def obtenClienteOpenAI():
     if local_check():
         print("Estoy en LOCAL...")
+        time.sleep(18)
         import bridges
         buzz = bridges.buzz
         llave = bridges.llave
     else:
         print("Estoy en REMOTO...")
+        time.sleep(18)
         buzz = os.getenv("buzz")
         llave = os.getenv("llave")
 
@@ -19,6 +22,8 @@ def obtenClienteOpenAI():
 def local_check(): 
 
     hostname = socket.gethostname()
+    print("Dentro de local_check... , el hostname es: ", hostname)
+    time.sleep(18)
 
     if hostname == 'nombre_del_servidor':
         print("Ejecutando en el servidor")
