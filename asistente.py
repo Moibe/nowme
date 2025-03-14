131309415615050dfaacb62784ce70cf9938ba4d
Moibe <moi.estrello@gmail.com>
2025-03-14 16:51:00 -0600

    Asistente 3 de alimentos, back a original

--- asistente.py
+++ asistente.py
@@ -1,7 +1,8 @@
 macronutrientes = "asst_9JBQUaVq1Z2hsEhWIub9skHl"
 comidas = "asst_EJIfpgocC28VHCw2B6V6MduR"
 
-alimentos = "asst_CjpwbK3oURVXkrkCz00TxTRv"
+#alimentos = "asst_CjpwbK3oURVXkrkCz00TxTRv"
+alimentos = "asst_Lj6Qr0qhHnOsnIeErMWf9BYd"
 receta = "asst_b48NBLlvk7cXUdp8DcLxAvsM"
 
 sustitucion = "asst_YsHcJZ5IBpRkVM4M6Wmf5d2w"
