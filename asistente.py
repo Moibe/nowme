ad4c8fe965dd22c8639dcfd0650db24efdd2f632
Moibe <moi.estrello@gmail.com>
2025-03-20 11:47:50 -0600

    Asistentes para Entrenamiento

--- asistente.py
+++ asistente.py
@@ -1,8 +1,14 @@
+#ASISTENTES DE NUTRICIÓN
 macronutrientes = "asst_9JBQUaVq1Z2hsEhWIub9skHl"
 comidas = "asst_EJIfpgocC28VHCw2B6V6MduR"
-
-#alimentos = "asst_CjpwbK3oURVXkrkCz00TxTRv"
+#alimentos = "asst_CjpwbK3oURVXkrkCz00TxTRv" #Modificado
 alimentos = "asst_Lj6Qr0qhHnOsnIeErMWf9BYd"
 receta = "asst_b48NBLlvk7cXUdp8DcLxAvsM"
-
 sustitucion = "asst_YsHcJZ5IBpRkVM4M6Wmf5d2w"
+
+#ASISTENTES DE ENTRENAMIENTO
+grupos_musculares = "asst_ysObLKTwE1kD5XvwAhRMZr78"
+eleccion_ejercicios = "asst_ZM8ulY0uGeF9TvX6YqKzdVmw"
+distribucion_bloques = "asst_Vd9rd1XL5gEMqd2Rud0jgvu4"
+distribucion_circuito = "asst_llslx5MnftjRitn8FmM0JS5b"
+distribucion_sumatoria = "asst_T3de7egZzBBEmgLJlWaAKcDr"
