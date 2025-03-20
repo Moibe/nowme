2df07f7de5738af170533beebb40cfc64aab8f99
Moibe <moi.estrello@gmail.com>
2025-03-20 12:30:24 -0600

    Se agregan endpoints para Entrenamientos

--- funciones.py
+++ funciones.py
@@ -1,6 +1,7 @@
 import asistente
 import operacionesIA
 
+#Nutrición 
 
 def getMacronutrientes(prompt):
     return operacionesIA.consulta(asistente.macronutrientes, prompt)
@@ -16,3 +17,20 @@ def getReceta(prompt):
 
 def getSustitucion(prompt):
     return operacionesIA.consulta(asistente.sustitucion, prompt)
+
+#Entrenamiento
+
+def getDistribucionGruposMusculares(prompt):
+    return operacionesIA.consulta(asistente.grupos_musculares, prompt)
+
+def getEjercicios(prompt):
+    return operacionesIA.consulta(asistente.eleccion_ejercicios, prompt)
+
+def getBloques(prompt):
+    return operacionesIA.consulta(asistente.distribucion_bloques, prompt)
+
+def getCircuito(prompt):
+    return operacionesIA.consulta(asistente.distribucion_circuito, prompt)
+
+def getSumatoria(prompt):
+    return operacionesIA.consulta(asistente.distribucion_sumatoria, prompt)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#NUTRICIÓN
+
 #1.- Obtener distribución macronutrientes.
 @app.post(
         "/macronutrientes/",
@@ -39,6 +41,48 @@ async def receta(prompt: str):
 @app.post(
         "/sustituir_alimento/",
         tags=["Nutrición"],
-        summary="Nutrición: 5.- Sustituir alimento ")
+        summary="Nutrición: 5.- Sustituir Alimento ")
 async def sustituir_alimento(prompt: str):
     return funciones.getSustitucion(prompt)
+
+#ENTRENAMIENTO
+
+#1.- Obtener distribución de grupos musculares.
+@app.post(
+        "/distribucion_musculos/",
+        tags=["Entrenamiento"],
+        summary="1.- Obtener Distribución de Grupos Musculares")
+async def macronutrientes(prompt: str):
+    return funciones.getDistribucionGruposMusculares(prompt)
+
+#2.- Obtener ejercicios.
+@app.post(
+        "/ejercicios/",
+        tags=["Entrenamiento"],
+        summary="2.- Obtener Selección de Ejercicios")
+async def macronutrientes(prompt: str):
+    return funciones.getEjercicios(prompt)
+
+#3.- Obtener distribución por bloques.
+@app.post(
+        "/bloques/",
+        tags=["Entrenamiento"],
+        summary="3A.- Obtener Distribución de Bloques")
+async def macronutrientes(prompt: str):
+    return funciones.getBloques(prompt)
+
+#4.- Obtener distribución en circuito.
+@app.post(
+        "/circuito/",
+        tags=["Entrenamiento"],
+        summary="3B.- Obtener Distribución en Circuito")
+async def macronutrientes(prompt: str):
+    return funciones.getCircuito(prompt)
+
+#5.- Obtener distribución en sumatoria.
+@app.post(
+        "/sumatoria/",
+        tags=["Entrenamiento"],
+        summary="3C.- Obtener Distribución en Sumatoria")
+async def macronutrientes(prompt: str):
+    return funciones.getSumatoria(prompt)
